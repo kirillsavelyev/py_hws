d1330f7815fe964e0c4d298d2337a015d6142992
Kirill Savelyev <kirillsavelyev@yandex.ru>
2017-02-18 22:17:47 +0300

    pep8 hw5.py

--- tceh_cource3/hw5.py
+++ tceh_cource3/hw5.py
@@ -1,66 +1,79 @@
 # -*- coding: utf-8 -*-
 
-#Написать декоратор, который отменяет выполнение функции и пишет: ИМЯ_ФУНКЦИИ не будет вызвана
+from time import process_time
+from random import random
+from datetime import date, timedelta
+
+# Написать декоратор, который отменяет выполнение функции и пишет: ИМЯ_ФУНКЦИИ не будет вызвана
 print('01\n')
 
-def cancelRun(func):
-	print (func.__name__ + u' не будет вызвана')
 
-@cancelRun
-def noPensions():
+def cancelrun(func):
+	print(func.__name__ + u' не будет вызвана')
+
+
+@cancelrun
+def nopensions():
 	print(u"Здоровья вам и хорошего настроения! © ДАМ")
 
 
-#Реализовать декоратор, который измеряет скорость выполнения функций. Написать три разные функции, задекорировать их и проверить
+# Реализовать декоратор, который измеряет скорость выполнения функций. Написать три разные
+# функции, задекорировать их и проверить
 print('\n02\n')
 
-from time import clock
 
-def measureSpeed(func):
+def measurespeed(func):
+	def wrapper(*args, **kwargs):
+		t = process_time()
+		res = func(*args, **kwargs)
+		print(func.__name__ + ' running time:', process_time() - t)
+		return res
 
-    def wrapper(*args, **kwargs):
-        t = clock()
-        res = func(*args, **kwargs)
-        print (func.__name__ + ' running time:', clock() - t)
-        return res
-    return wrapper
+	return wrapper
 
-@measureSpeed
+
+@measurespeed
 def factorial(n):
 	n = int(n)
 	fctrl = 1
-	i = 0
-	while i < n:
-		i += 1
-		fctrl = fctrl * i
+	m = 0
+	while m < n:
+		m += 1
+	fctrl = fctrl * m
+	print(fctrl)
 
-	#print(fctrl)
 
-poem = list('I went down to the river, I set down on the bank. I tried to think but couldn\'t,So I jumped in and sank.')
+poem = list('I went down to the river, I set down on the bank. I tried to think but couldn\'t,'
+			' So I jumped in and sank.')
 
-@measureSpeed
-def insertToList(i, item):
-	poem.insert(i, item)
-	#print ('Ta-daa!')
 
-@measureSpeed
-def appendToList(item):
+@measurespeed
+def inserttolist(key, item):
+	poem.insert(key, item)
+
+
+# print ('Ta-daa!')
+
+
+@measurespeed
+def appendtolist(item):
 	poem.append(item)
-	#print (poem)
+
+
+# print (poem)
 
 factorial(9999)
-insertToList(30, 'WOW!')
-appendToList('WOW!')
+inserttolist(30, 'WOW!')
+appendtolist('WOW!')
 
-
-#Написать генероторное выражение, которое включает в себя все четные числа от 0 до 100
+# Написать генероторное выражение, которое включает в себя все четные числа от 0 до 100
 print('\n03\n')
 
 generator = (x for x in range(2, 101, 2))
 print(list(generator))
 
-
-#Написать генератор, который возвращает бесконечную последовательность случайных чисел, таких что следующее не меньше прошлого
+# Написать генератор, который возвращает бесконечную последовательность случайных чисел,
+# таких что следующее не меньше прошлого
 '''
 #Мне кажется перемудрил, т.к. генератор долго работает
 
@@ -77,36 +90,35 @@ def randomInfinity(x):
 randomInfinity(-1)
 '''
 
-#Быстро работает, но рамдомна только часть числа в последовательности
+# Быстро работает, но рамдомна только часть числа в последовательности
 print('\n04\n')
 
-from random import random
 
-def randomInfinity():
+def randominfinity():
 	t = 0
 	while True:
-		tmp = random() 
+		tmp = random()
 		t += tmp
 		yield t
 
-ri = randomInfinity()
+
+ri = randominfinity()
 
 for i in range(10):
 	print(next(ri))
 
-
-#Написать генератор, который принимает на вход дату и на каждый вызов выдает следующий день
+# Написать генератор, который принимает на вход дату и на каждый вызов выдает следующий день
 print('\n05\n')
 
-from datetime import date, timedelta
 
-def nexDayGenerator(y, m, d):
+def nexdaygenerator(y, m, d):
 	day = date(y, m, d)
 	while True:
 		day += timedelta(days=1)
 		yield day
 
-nd = nexDayGenerator(2017, 2, 23)
 
-for i in range(10):
-	print (next(nd))
+nd = nexdaygenerator(2017, 2, 23)
+
+for j in range(10):
+	print(next(nd))
